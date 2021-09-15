fa75d7d6799a6b6fa09949071f9345ba539d8635
KrutikGadhiya <66325627+KrutikGadhiya@users.noreply.github.com>
2021-09-15 19:15:16 +0530

    added week 2

---
+++ week2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Krutik Gadhiya</title>
+  <style>
+    * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+      font-family: 'Courier New', Courier, monospace;
+    }
+
+    body {
+      width: 100vw;
+      height: 100vh;
+      background: lightblue;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  </style>
+</head>
+
+<body>
+  <?php
+  echo "<h1>Krutik Gadhiya</h1>";
+  ?>
+</body>
+
+</html>
